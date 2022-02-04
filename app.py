b92151d100723d1ccd786014383547a476bad556
haruki <haruki24816@gmail.com>
2022-02-05 04:51:26 +0900

    ルームIDの確認機能を作った

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ import os
 import uuid
 from datetime import timedelta
 from flask import Flask, render_template, redirect, url_for, request, session
-from flask_socketio import SocketIO, emit, join_room
+from flask_socketio import SocketIO, emit, join_room, disconnect
 
 
 class Room:
 
-    def __init__(self, room_id, room_name):
+    def __init__(self, room_id, name):
         self.room_id = room_id
-        self.room_name = room_name
+        self.name = name
+        self.status = "募集中"
+        self.players = {}
 
 
 app = Flask(__name__)
@@ -26,7 +28,7 @@ def index():
 
 @app.route("/make_room")
 def make_room():
-    room_id = uuid.uuid1()
+    room_id = str(uuid.uuid1())
     room_name = request.args.get("room_name")
 
     if room_name in ("", None):
@@ -38,7 +40,38 @@ def make_room():
 
 @app.route("/<uuid:room_id>")
 def room(room_id):
+    room_id = str(room_id)
     return render_template("room.html", room_id=room_id)
+
+
+@socketio.on("connect")
+def on_connect(auth):
+    print("接続")
+
+
+@socketio.on("disconnect")
+def on_disconnect():
+    print("切断")
+
+
+@socketio.on("check_room_id")
+def check_room_id(data):
+    room_id = data["room_id"]
+
+    if room_id not in rooms:
+        emit("kick", {"message": "存在しない部屋です"})
+        disconnect()
+        return
+
+    room = rooms[room_id]
+
+    if room.status != "募集中":
+        emit("kick", {"message": "この部屋は現在募集中ではありません"})
+        disconnect()
+        return
+
+    room.players[request.sid] = ""
+    emit("ask_player_name", {"room_name": room.name})
 
 
 if __name__ == "__main__":
