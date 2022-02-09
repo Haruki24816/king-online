5734c4ac23959fbb642debf02827936a0a7c10ea
haruki <haruki24816@gmail.com>
2022-02-09 16:22:52 +0900

    リファクタリング

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 import os
 import uuid
 from datetime import timedelta
-from flask import Flask, render_template, redirect, url_for, request, session
-from flask_socketio import SocketIO, emit, join_room, disconnect
+from flask import Flask, render_template, redirect, request, session
+from flask_socketio import SocketIO, emit, join_room
 
 
 class Room:
 
-    def __init__(self, room_id, name):
-        self.room_id = room_id
+    def __init__(self, name):
         self.name = name
-        self.status = "募集中"
-        self.players = {}
 
 
 app = Flask(__name__)
@@ -28,51 +25,20 @@ def index():
 
 @app.route("/make_room")
 def make_room():
-    room_id = str(uuid.uuid1())
-    room_name = request.args.get("room_name")
+    room_id = uuid.uuid1()
+    name = request.args.get("name")
 
-    if room_name in ("", None):
-        room_name = "名前のない部屋"
+    if name in ("", None):
+        name = "名前のない部屋"
 
-    rooms[room_id] = Room(room_id, room_name)
+    rooms[room_id] = Room(name)
     return redirect(f"/{room_id}")
 
 
 @app.route("/<uuid:room_id>")
 def room(room_id):
-    room_id = str(room_id)
     return render_template("room.html", room_id=room_id)
 
 
-@socketio.on("connect")
-def on_connect(auth):
-    print("接続")
-
-
-@socketio.on("disconnect")
-def on_disconnect():
-    print("切断")
-
-
-@socketio.on("check_room_id")
-def check_room_id(data):
-    room_id = data["room_id"]
-
-    if room_id not in rooms:
-        emit("kick", {"message": "存在しない部屋です"})
-        disconnect()
-        return
-
-    room = rooms[room_id]
-
-    if room.status != "募集中":
-        emit("kick", {"message": "この部屋は現在募集中ではありません"})
-        disconnect()
-        return
-
-    room.players[request.sid] = ""
-    emit("ask_player_name", {"room_name": room.name})
-
-
 if __name__ == "__main__":
-    socketio.run(app)
+    socketio.run(app, debug=True)
--- static/js/room.js
+++ static/js/room.js
@@ -1,18 +1,1 @@
 var socket = io();
-
-socket.on("connect", function() {
-  socket.emit("check_room_id", {"room_id": room_id});
-  document.getElementById("connection_status").innerText = "ルームID確認中";
-});
-
-socket.on("kick", function(data) {
-  document.getElementById("connection_status").innerText = "切断されました：" + data["message"];
-});
-
-socket.on("ask_player_name", function(data) {
-  document.getElementById("connection_status").innerText = "接続中";
-  document.getElementById("room_name").innerText = data["room_name"];
-  document.getElementById("room_description").innerText ="募集中";
-  document.getElementById("connection_status").classList.add("hidden");
-  document.getElementById("header").classList.remove("hidden");
-});
