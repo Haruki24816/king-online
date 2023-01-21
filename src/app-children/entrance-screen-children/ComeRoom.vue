d06b94f02135d68b40f5e0186640d8f36a10c3b3
Haruki <haruki24816@gmail.com>
2023-01-21 22:33:27 +0900

    ComeRoom.vueを編集

--- src/app-children/entrance-screen-children/ComeRoom.vue
+++ src/app-children/entrance-screen-children/ComeRoom.vue
@@ -1,3 +1,13 @@
 <template>
-  a
+  <div class="form-floating mb-3">
+    <input type="text" class="form-control" id="form-id" placeholder="">
+    <label for="form-id">部屋のID</label>
+  </div>
+  <div class="form-floating mb-3">
+    <input type="text" class="form-control" id="form-name" placeholder="">
+    <label for="form-name">あなたの名前</label>
+  </div>
+  <div class="d-grid gap-2">
+    <button class="btn btn-primary disabled">入室</button>
+  </div>
 </template>
