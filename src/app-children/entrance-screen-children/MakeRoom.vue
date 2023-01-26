4eff439bf8018f0c5293471a0aa777621ac95099
Haruki <haruki24816@gmail.com>
2023-01-26 22:14:19 +0900

    MakeRoom.vueを変更

--- src/app-children/entrance-screen-children/MakeRoom.vue
+++ src/app-children/entrance-screen-children/MakeRoom.vue
@@ -1,3 +1,13 @@
 <template>
-  a
+  <div class="form-floating mb-3">
+    <input type="text" class="form-control" id="form-room-name" placeholder="">
+    <label for="form-room-name">部屋の名前</label>
+  </div>
+  <div class="form-floating mb-3">
+    <input type="text" class="form-control" id="form-name" placeholder="">
+    <label for="form-name">あなたの名前</label>
+  </div>
+  <div class="d-grid gap-2">
+    <button class="btn btn-primary disabled">作成</button>
+  </div>
 </template>
