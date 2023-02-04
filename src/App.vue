8c04ce88c4011ffcbe0f868183afd66d3a802589
Haruki <haruki24816@gmail.com>
2023-02-04 21:26:04 +0900

    socket.ioの準備

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup>
+import { inject } from "vue"
 import { store0 } from "/src/store0.js"
 import OutsideScreen from "./app-children/OutsideScreen.vue"
 import InsideScreen from "./app-children/InsideScreen.vue"
+
+const $socket = inject("$socket")
+
+$socket.on("connect", () => {
+  console.log("connect")
+})
+
+$socket.on("disconnect", () => {
+  console.log("disconnect")
+})
 </script>
 
 <template>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from "vue"
 import "./styles.scss"
 import * as bootstrap from "bootstrap"
 import "bootstrap-icons/font/bootstrap-icons.css"
+import io from "socket.io-client"
 import App from "./App.vue"
 
-createApp(App).mount("#app")
+const app = createApp(App)
+app.config.globalProperties.$socket = io()
+app.provide("$socket", app.config.globalProperties.$socket)
+app.mount("#app")
