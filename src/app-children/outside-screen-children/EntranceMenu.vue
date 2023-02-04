f05d97806f5b57bc9dcb304a6e99ec5b3c6b8e95
Haruki <haruki24816@gmail.com>
2023-02-04 13:27:43 +0900

    とりあえずモーダル追加

--- src/app-children/outside-screen-children/EntranceMenu.vue
+++ src/app-children/outside-screen-children/EntranceMenu.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="mt-3"><button type="button" class="btn btn-dark">部屋に入る</button></div>
-  <div class="mt-1"><button type="button" class="btn btn-dark">部屋を作る</button></div>
-  <div class="mt-1"><button type="button" class="btn btn-dark">このサイトについて</button></div>
+  <div class="mt-3"><button type="button" class="btn btn-dark" data-bs-toggle="modal"
+      data-bs-target="#XXX">部屋に入る</button></div>
+  <div class="mt-1"><button type="button" class="btn btn-dark" data-bs-toggle="modal"
+      data-bs-target="#XXX">部屋を作る</button></div>
+  <div class="mt-1"><button type="button" class="btn btn-dark" data-bs-toggle="modal"
+      data-bs-target="#XXX">このサイトについて</button></div>
 </template>
--- src/store0.js
+++ src/store0.js
@@ -1,8 +1,8 @@
 import { ref } from "vue"
 
 export const store0 = ref({
-  appMode: "inside", // outside, inside
-  outsideMode: "message", // splash, menu, message
+  appMode: "outside", // outside, inside
+  outsideMode: "menu", // splash, menu, message
   roomName: "家庭的な女がタイプの俺 パスタ作ったお前 大貧民負けてマジ切れ パスタ作ったお前",
   roomInfo: {"項目1": "内容", "項目2": "内容"},
   disconnectionReason: "切断理由"
