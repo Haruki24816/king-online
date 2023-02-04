4c9f68e96909e825c881952290492c2801a6e441
Haruki <haruki24816@gmail.com>
2023-02-04 12:37:33 +0900

    メッセージ画面を編集

--- src/app-children/outside-screen-children/MessageDisplay.vue
+++ src/app-children/outside-screen-children/MessageDisplay.vue
@@ -1,5 +1,9 @@
+<script setup>
+import { store0 } from "/src/store0.js"
+</script>
+
 <template>
-  <h3 class="text-white mt-4">切断されました</h3>
-  <p class="text-white">切断理由</p>
+  <h3 class="text-white mt-4">退室しました</h3>
+  <p class="text-white">{{ store0.disconnectionReason }}</p>
   <button type="button" class="btn btn-dark">戻る</button>
 </template>
--- src/store0.js
+++ src/store0.js
@@ -1,8 +1,9 @@
 import { ref } from "vue"
 
 export const store0 = ref({
-  appMode: "inside", // outside, inside
+  appMode: "outside", // outside, inside
   outsideMode: "message", // splash, menu, message
   roomName: "きのこの山",
-  roomInfo: {"項目1": "内容", "項目2": "内容"}
+  roomInfo: {"項目1": "内容", "項目2": "内容"},
+  disconnectionReason: "切断理由"
 })
