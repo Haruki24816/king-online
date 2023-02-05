5351d07e1c162115e9ef8fb8c1f3c6579415f455
Haruki <haruki24816@gmail.com>
2023-02-05 13:18:40 +0900

    s0-failed-enter-roomを実装

--- src/app-children/outside-screen-children/EnterRoom.vue
+++ src/app-children/outside-screen-children/EnterRoom.vue
@@ -6,6 +6,8 @@ const $socket = inject("$socket")
 
 const roomId = ref("")
 const playerName = ref("")
+const reason = ref("")
+const alert = ref(false)
 
 const buttonClass = computed(() => {
   if (roomId.value.length == 8 &&
@@ -21,11 +23,22 @@ const buttonClass = computed(() => {
 
 function enterRoom() {
   store0.value.entranceLock = true
+  alert.value = false
   $socket.emit("c0-enter-room", { "room_id": roomId, "player_name": playerName })
 }
+
+$socket.on("s0-failed-enter-room", (data) => {
+  reason.value = data.reason
+  alert.value = true
+  store0.value.entranceLock = false
+})
 </script>
 
 <template>
+  <div class="alert alert-danger d-flex align-items-center" v-if="alert">
+    <i class="bi bi-exclamation-triangle-fill fs-4 me-3"></i>
+    <div>{{ reason }}</div>
+  </div>
   <div class="form-floating mb-3">
     <input type="text" class="form-control" id="enterRoomId" placeholder="" v-model="roomId">
     <label for="enterRoomId">部屋のID</label>
