ac08e4dd4218909a2a23c053835e8d85e207cd02
Haruki <haruki24816@gmail.com>
2023-02-05 13:23:47 +0900

    s0-failed-make-roomを実装

--- src/app-children/outside-screen-children/MakeRoom.vue
+++ src/app-children/outside-screen-children/MakeRoom.vue
@@ -6,6 +6,8 @@ const $socket = inject("$socket")
 
 const roomName = ref("")
 const ownerName = ref("")
+const reason = ref("")
+const alert = ref(false)
 
 const buttonClass = computed(() => {
   if (1 <= roomName.value.length &&
@@ -22,11 +24,22 @@ const buttonClass = computed(() => {
 
 function makeRoom() {
   store0.value.entranceLock = true
+  alert.value = false
   $socket.emit("c0-make-room", { "room_name": roomName, "owner_name": ownerName })
 }
+
+$socket.on("s0-failed-make-room", (data) => {
+  reason.value = data.reason
+  alert.value = true
+  store0.value.entranceLock = false
+})
 </script>
 
 <template>
+  <div class="alert alert-danger d-flex align-items-center" v-if="alert">
+    <i class="bi bi-exclamation-triangle-fill fs-4 me-3"></i>
+    <div>{{ reason }}</div>
+  </div>
   <div class="form-floating mb-3">
     <input type="text" class="form-control" id="makeRoomName" placeholder="" v-model="roomName">
     <label for="makeRoomName">部屋の名前</label>
