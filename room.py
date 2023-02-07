f35a61eea571ea16db0dc237a84d6e7e343fa52b
Haruki <haruki24816@gmail.com>
2023-02-07 11:30:03 +0900

    プレイヤークラスを削除

--- room.py
+++ room.py
@@ -1,6 +1,3 @@
-from player import Player
-
-
 class Room:
 
     def __init__(self, room_name):
@@ -14,21 +11,37 @@ class Room:
         self.players = []
 
     def add_player(self, player_name):
-        for player in self.players:
-            if player_name == player.name:
+        for player_data in self.players:
+            if player_data["name"] == player_name:
                 raise Exception("すでに同じ名前のプレイヤーがいます")
-        
-        self.players.append(Player(player_name))
+
+        if len(player_name) == 0:
+            raise Exception("プレイヤー名は1文字以上でなければなりません")
+
+        if 100 < len(player_name):
+            raise Exception("プレイヤー名は100文字以下でなければなりません")
+
+        self.players.append({
+            "name": player_name,
+            "status": "online" # online, offline, left
+        })
+
         return len(self.players) - 1
-    
+
     def leave(self, player_id):
-        self.players[player_id].leave()
-    
+        self.players[player_id]["status"] = "left"
+
     def offline(self, player_id):
-        self.players[player_id].offline()
-    
+        self.players[player_id]["status"] = "offline"
+
     def is_offline(self, player_id):
-        return self.players[player_id].is_offline()
-    
+        return self.players[player_id]["status"] == "offline"
+
     def reconnect(self, player_id):
-        return self.players[player_id].reconnect()
+        status = self.players[player_id]["status"]
+
+        if status == "left":
+            return False
+        else:
+            status = "online"
+            return True
