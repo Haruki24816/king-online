9d04db4f24347d443a64a60fc4d1fcbdb166af64
Haruki <haruki24816@gmail.com>
2023-02-17 20:04:59 +0900

    リファクタリング

--- src/app-children/OutsideScreen.vue
+++ src/app-children/OutsideScreen.vue
@@ -68,19 +68,7 @@ function reload() {
       </div>
     </div>
   </div>
-  <div class="modal fade" id="aboutModal" tabindex="-1">
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="aboutModalLabel">このサイトについて</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-        </div>
-        <div class="modal-body">
-          <About />
-        </div>
-      </div>
-    </div>
-  </div>
+  <About />
 </template>
 
 <style scoped>
--- src/app-children/outside-screen-children/About.vue
+++ src/app-children/outside-screen-children/About.vue
@@ -1,3 +1,14 @@
+<script setup>
+import ModalBase from "/src/base-components/ModalBase.vue"
+</script>
+
 <template>
-  <a href="https://toranpu.wiki.fc2.com/wiki/お金（銀行）">トランプゲーム「銀行」</a>をもとに考案された、オリジナルトランプゲーム「キング」のオンラインゲームです。
+  <ModalBase name="about" title="このサイトについて" num="0">
+    <div class="modal-body">
+      <p>
+        <a href="https://toranpu.wiki.fc2.com/wiki/お金（銀行）">トランプゲーム「銀行」</a>
+        をもとに考案された、オリジナルトランプゲーム「キング」のオンラインゲームです。
+      </p>
+    </div>
+  </ModalBase>
 </template>
